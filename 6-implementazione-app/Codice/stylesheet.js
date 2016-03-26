69db4b4de438ccd57fbed4b34d7e16a934c2d388
Valerio Cassani <valerio.cassani@gmail.com>
2016-03-26 19:10:13 +0100

    passaggio listati a minted

---
+++ 6-implementazione-app/Codice/stylesheet.js
@@ -0,0 +1,25 @@
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    marginTop: 20
+  },
+  iosContainer: {
+    backgroundColor: 'white',
+    marginTop: 70
+  },
+  iosStatusBar:{
+    color: 'black'
+  },
+  buttonMaterial: {
+    height: 36,
+    backgroundColor: primary_color,
+    borderColor: primary_color,
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  }
+  //altri elementi di stile
+})   
